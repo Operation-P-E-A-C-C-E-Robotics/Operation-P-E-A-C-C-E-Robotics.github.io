668d76369ed9a12c6e44424c6738166073b3b088
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-04-15 21:28:44 -0400

    testing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const tba = require("tba-js");
 
+tba.setAppID("frc4909:tba-api:v0.0.1");
+
 tba.getMatches("2016", "casd", (data) =>{
     console.dir(data);
 });
