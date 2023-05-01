15e5e5439db7e0d0be9c37f89932ba3eb871ba87
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-05-01 16:52:11 -0400

    test pythong runner

---
+++ tba.py
@@ -0,0 +1,20 @@
+from num2words import num2words
+import aiohttp
+import aiotba
+import env
+
+tba = env.tba_api_key
+
+async def session():
+
+    async with aiohttp.ClientSession() as http_session:     
+     tbaSession = aiotba.TBASession(tba, http_session)
+     
+     #  Thread(target=loop.run_forever).start()
+     print("Session Function has been executed")
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(session())
+    return tbaSession
+
+
+print("Script Ran")
