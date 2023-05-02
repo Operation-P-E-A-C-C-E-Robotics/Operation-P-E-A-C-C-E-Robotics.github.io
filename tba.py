58d49273e8edbd6d67ce7f4b852ed7daa6723ae8
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-05-01 23:19:55 -0400

    need to see where I am

--- tba.py
+++ tba.py
@@ -23,6 +23,7 @@ async def season(api):
     events = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/events/{year}?X-TBA-Auth-Key={api}')
     # print(tba_api_key)
     print(events.text)
+    print(os.curdir)
     with open("./events.json", "w") as outfile:
         json.dump(events.json(), outfile)
         outfile.close()
