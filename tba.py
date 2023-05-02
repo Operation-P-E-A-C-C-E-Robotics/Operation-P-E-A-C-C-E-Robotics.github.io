0ac86d9c1267d6623593f46cf3036885f0690bd2
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-05-01 21:24:50 -0400

    test print

--- tba.py
+++ tba.py
@@ -6,21 +6,34 @@ import os
 tba_api_key = os.getenv('TBA_API_KEY')
 
 
-async def session():
-
+async def matches(ctx, event:str, team:int):
     async with aiohttp.ClientSession() as http_session:     
-     tbaSession = aiotba.TBASession(tba_api_key, http_session)
-     
-     #  Thread(target=loop.run_forever).start()
-     print("Session Function has been executed")
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(session())
-    return tbaSession
+        tbaSession = aiotba.TBASession(tba, http_session)
+        msg = await ctx.send("Processing...")
+        eventdata = await tbaSession.event(event_key=event)
+        stats = await tbaSession.team_event_matches(team=team, event=event)
+        # embed = discord.Embed(title=f"{eventdata.name} {eventdata.year}", description=f"Week {eventdata.week} {eventdata.event_type_string} Event")
+        # embed.set_thumbnail(url='https://frcavatars.herokuapp.com/get_image?team={}'.format(team))
+        for m in stats: 
+            matchdata = await tbaSession.match(m)
+            matchtitle=""
+            if (matchdata.comp_level == "qm"):
+                matchtitle = "Qualification"
+            elif (matchdata.comp_level == "qf"):
+                matchtitle = f"**Quarterfinal** {matchdata.set_number} Match"
+            elif (matchdata.comp_level == "sf"):
+                matchtitle = f"**Semifinal** {matchdata.set_number} Match"
+            elif (matchdata.comp_level == "f"):
+                matchtitle = f"**Final** {matchdata.set_number} Match"
+            else:
+                matchtitle = "Unknown"
+
+            matchlvl = str('**Red:** {} \n **Blue:** {} \n **Winner:** {}').format('-'.join(matchdata.alliances.get("red").team_keys).replace('frc', ''), '-'.join(matchdata.alliances.get("blue").team_keys).replace('frc', ''), matchdata.winning_alliance.capitalize())
+            ##(m)
+            #level.append(matchlvl)
+            # embed.add_field(name=f"{matchtitle} {m.match_number}", value=f"{matchlvl.replace(f'{team}', f'**{team}**')}", inline=True)
+        # await ctx.send(embed=embed)
+        # await msg.delete()
+        print(stats)
 
 
-def getAllMatches(tba_api_key):
-    tba = session()
-    print(tba.event_teams_statuses("2023ctwat"))
-
-
-getAllMatches(tba_api_key)
