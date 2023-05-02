095b194f8fe301ef1ec9670d57453d5624e62d4c
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-05-01 23:10:39 -0400

    test

--- tba.py
+++ tba.py
@@ -23,6 +23,8 @@ async def season(api):
     events = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/events/{year}?X-TBA-Auth-Key={api}')
     # print(tba_api_key)
     print(events.text)
+    with open("events.json", "w") as outfile:
+        json.dump(events.text, outfile)
 
 
 
