c26e7a4c595ea56a231c7fcecf70cb26d1d567bd
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-05-22 21:56:31 -0400

    stash

--- assets/js/googleCalendar.js
+++ assets/js/googleCalendar.js
@@ -3,6 +3,7 @@ const staticURL = "https://raw.githubusercontent.com/Operation-P-E-A-C-C-E-Robot
 async function getEventsJson() {
     const response = await fetch(staticURL);
     const eventsJson = await response.json();
+    console.log(eventsJson)
     return eventsJson;
 }
 
@@ -23,5 +24,39 @@ async function getNextNonRecurringEvent() {
     }
     console.log(nextEvent)
     return Promise.resolve(nextEvent);
-  }
+}
   
+
+
+async function getNextEvent() {
+    events = getEventsJson();
+  
+    // Get the current date/time
+    const currentDate = new Date();
+  
+    // Initialize variables for storing the next event information
+    let nextEvent = null;
+    let nextEventDate = null;
+  
+    // Iterate over the events and find the next upcoming event
+    for (const event of events) {
+      const eventDate = new Date(event.start.dateTime);
+  
+      // Check if the event date is in the future and update the next event information if applicable
+      if (eventDate > currentDate && (!nextEventDate || eventDate < nextEventDate)) {
+        nextEvent = event;
+        nextEventDate = eventDate;
+      }
+    }
+  
+    return nextEvent;
+  }
+
+function updateHomePageEventBox(data) {
+  date = new Date(data.start.dateTime)
+  console.log(date)
+  document.getElementById('nextEvent').innerText = ' ' + data.summary
+  document.getElementById('startTime').innerHTML += ' ' + date.toLocaleString('en-US', options)
+  document.getElementById('location').innerHTML += ' ' + data.location
+  document.getElementById('goToCalendar').href = data.htmlLink
+}
