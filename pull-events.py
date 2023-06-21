9b59410b654012f1f93f17b56d3cd7c4d4910974
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-06-21 12:06:22 -0400

    testing

--- pull-events.py
+++ pull-events.py
@@ -2,7 +2,11 @@ import json
 import os
 import requests
 import datetime
+from dateutil.rrule import rrulestr
+from dateutil.parser import parse
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Set the Google Calendar API endpoint URL
 calendar_url = "https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events"
@@ -11,7 +15,14 @@ calendar_url = "https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/e
 calendar_id = "team@peacce.org"
 
 # Set your Google API key
+# try:
 api_key = os.environ['API_KEY']
+print('Using environ API Key for Production')
+# except:
+#     api_key = os.getenv('API_KEY')
+#     print('Using env file API Key for Testing')
+#     print(api_key)
+#     print("heresy")
 
 # Get the current date and time
 now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
@@ -25,13 +36,52 @@ params = {
     'timeMax': max,
 }
 
+
+def get_recurrence_occurrences(event, recurrence_rule):
+    start_datetime = parse(event['start']['dateTime'])
+    end_datetime = parse(event['end']['dateTime'])
+
+    # Parse the recurrence rule
+    rule = rrulestr(recurrence_rule, dtstart=start_datetime)
+
+    # Generate occurrences based on the rule
+    occurrences = []
+    for occurrence in rule:
+        occurrence_event = event.copy()
+        occurrence_event['start']['dateTime'] = occurrence.isoformat()
+        occurrence_event['end']['dateTime'] = (occurrence + (end_datetime - start_datetime)).isoformat()
+        occurrences.append(occurrence_event)
+
+    return occurrences
+
+
 # Make the API request
 response = requests.get(calendar_url.format(calendar_id=calendar_id), params=params)
 
 # Parse the response JSON
 events = response.json()["items"]
 
-# Save the events to a JSON file
+# Create a list to store individual event objects
+individual_events = []
+
+# Process each event
+for event in events:
+    # Check if the event has a recurrence rule
+    if "recurrence" in event:
+        # Extract the recurrence rule
+        recurrence_rule = event["recurrence"][0]
+        
+        # Generate individual occurrences based on the recurrence rule
+        occurrences = get_recurrence_occurrences(event, recurrence_rule)
+        
+        # Add the individual occurrences to the list
+        individual_events.extend(occurrences)
+    else:
+        # Event has no recurrence rule, add it as is
+        individual_events.append(event)
+
+# Save the individual events to a JSON file
 with open("events.json", "w") as f:
-    # json.dump(events, f)
-    f.write(json.dumps(events, indent=4))
+    json.dump(individual_events, f, indent=4)
+
+
