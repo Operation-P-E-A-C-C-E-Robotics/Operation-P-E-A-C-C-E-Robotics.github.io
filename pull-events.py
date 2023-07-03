af81723cab76294efd7a1ae023ab00a3bcccda61
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-07-03 09:47:39 -0400

    stash

---
+++ pull-events.py
@@ -0,0 +1,37 @@
+import json
+import os
+import requests
+import datetime
+
+
+# Set the Google Calendar API endpoint URL
+calendar_url = "https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events"
+
+# Set the calendar ID for the public calendar you want to access
+calendar_id = "team@peacce.org"
+
+# Set your Google API key
+api_key = os.environ['API_KEY']
+
+# Get the current date and time
+now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+max = (datetime.datetime.utcnow() + datetime.timedelta(days=30)).isoformat() + 'Z'
+# Set the API request parameters
+params = {
+    "calendarId": calendar_id,
+    "key": api_key,
+    "timeZone": "UTC",
+    'timeMin': now,
+    'timeMax': max,
+}
+
+# Make the API request
+response = requests.get(calendar_url.format(calendar_id=calendar_id), params=params)
+
+# Parse the response JSON
+events = response.json()["items"]
+
+# Save the events to a JSON file
+with open("events.json", "w") as f:
+    # json.dump(events, f)
+    f.write(json.dumps(events, indent=4))
