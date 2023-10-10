5c41e2ab18a240ba995b5aca09b85059750927ae
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-10-09 23:54:28 -0400

    add kickoff count down

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -6,3 +6,21 @@ function showAlert(obj){
 
     $('#alert').append(html);
 }
+
+function getFirstSaturdayInJanuary(year) {
+  const january = 0; // January is month 0
+  // const year = year; // Get the current year
+
+  // Loop through the days of January until you find a Saturday that is not January 1st
+  for (let day = 2; day <= 7; day++) { // Start from January 2nd (day 2) and go up to January 7th (day 7)
+    const date = new Date(year, january, day);
+
+    // Check if the day is a Saturday (where 0 is Sunday and 6 is Saturday) and not January 1st
+    if (date.getDay() === 6 && day !== 1) {
+      return date; // Found the first Saturday in January that is not New Year's Day
+    }
+  }
+
+  // If no suitable date is found, you can return a message or handle it as needed
+  return "No suitable date found in January.";
+}
