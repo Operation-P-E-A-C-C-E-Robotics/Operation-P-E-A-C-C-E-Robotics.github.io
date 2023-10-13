e11fa43cae5068502803ba7a9d85995d80040535
Creeper120-DCUStudios <boyspockie@gmail.com>
2023-10-13 19:12:49 -0400

    Dark Mode Button

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -24,3 +24,42 @@ function getFirstSaturdayInJanuary(year) {
   // If no suitable date is found, you can return a message or handle it as needed
   return "No suitable date found in January.";
 }
+
+
+ 
+
+
+function toggleDarkMode() {
+  var isDarkMode = document.body.classList.contains('bg-dark');
+
+  // Toggle Bootstrap 4 classes for background and text color
+  if (isDarkMode) {
+      document.body.classList.remove('bg-dark', 'text-light');
+      document.body.classList.add('bg-light', 'text-dark');
+      localStorage.setItem('bg-dark', 'false'); // Store as a string
+  } else {
+      document.body.classList.add('bg-dark', 'text-light');
+      document.body.classList.remove('bg-light', 'text-dark');
+      localStorage.setItem('bg-dark', 'true'); // Store as a string
+  }
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const myButton = document.getElementById('darkModeToggle');
+  // Retrieve the dark mode preference from localStorage on page load
+  var isDarkModePreference = localStorage.getItem('bg-dark');
+
+  if (isDarkModePreference === 'true') {
+    toggleDarkMode(); // If the preference is 'true', set dark mode
+  }
+  // Define the function you want to run when the button is pressed
+  function handleClick() {
+      alert('Button Clicked!'); // You can replace this with your custom code.
+  }
+
+  // Add a click event listener to the button
+  myButton.addEventListener('click', toggleDarkMode);
+});
