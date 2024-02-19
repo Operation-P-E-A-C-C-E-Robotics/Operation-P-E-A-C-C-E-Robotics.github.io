083afc72fd02e3d7f0e5be83c8b716c5c15a64a8
Creeper120-DCUStudios <boyspockie@gmail.com>
2024-02-18 20:39:15 -0500

    re-enable years sponsored after testing

--- tina/config.ts
+++ tina/config.ts
@@ -38,12 +38,12 @@ import { date } from "zod";
     };
   })
   
-  for (let i = 2011; i <= new Date().getFullYear(); i++) {
-    yearOptions.push({
-      label: i.toString(), // Convert the number to a string
-      value: i.toString(), // Convert the number to a string
-    });
-  }
+  // for (let i = 2011; i <= new Date().getFullYear(); i++) {
+  //   yearOptions.push({
+  //     label: i.toString(), // Convert the number to a string
+  //     value: i.toString(), // Convert the number to a string
+  //   });
+  // }
 
   console.log(yearOptions)
 
@@ -675,28 +675,28 @@ export default defineConfig({
             name: "image",
             type: "image"
           },
-          // {
-          //   label: "Years Sponsored",
-          //   name: "years",
-          //   type: "string",
-          //   // list: true,
-          //   // options: [  
-          //   //   { label: '2011', value: '2011' },
-          //   //   { label: '2012', value: '2012' },
-          //   //   { label: '2013', value: '2013' },
-          //   //   { label: '2014', value: '2014' },
-          //   //   { label: '2015', value: '2015' },
-          //   //   { label: '2016', value: '2016' },
-          //   //   { label: '2017', value: '2017' },
-          //   //   { label: '2018', value: '2018' },
-          //   //   { label: '2019', value: '2019' },
-          //   //   { label: '2020', value: '2020' },
-          //   //   { label: '2021', value: '2021' },
-          //   //   { label: '2022', value: '2022' },
-          //   //   { label: '2023', value: '2023' },
-          //   //   { label: '2024', value: '2024' },
-          //   //   ],
-          // },
+          {
+            label: "Years Sponsored",
+            name: "years",
+            type: "string",
+            list: true,
+            options: [  
+              { label: '2011', value: '2011' },
+              { label: '2012', value: '2012' },
+              { label: '2013', value: '2013' },
+              { label: '2014', value: '2014' },
+              { label: '2015', value: '2015' },
+              { label: '2016', value: '2016' },
+              { label: '2017', value: '2017' },
+              { label: '2018', value: '2018' },
+              { label: '2019', value: '2019' },
+              { label: '2020', value: '2020' },
+              { label: '2021', value: '2021' },
+              { label: '2022', value: '2022' },
+              { label: '2023', value: '2023' },
+              { label: '2024', value: '2024' },
+              ],
+          },
           {
             label: "Hide from Sponsor List",
             name:"hidden",
