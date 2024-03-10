a223a8315e66782d82e9a60bcaac9084e8fa6b90
Logan Ayers <boyspockie@gmail.com>
2024-03-10 13:06:41 -0400

    disable printing data to console

--- updateCurrentYear.py
+++ updateCurrentYear.py
@@ -51,16 +51,16 @@ def git_commit(files, message):
 async def season(api):
     year = datetime.date.today().year
     events = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/events/{year}?X-TBA-Auth-Key={api}')
-    # print(tba_api_key)
-    print(events.text)
+    # #print(tba_api_key)
+    #print(events.text)
     with open(f"{year}_events.json", "w") as outfile:
         outfile.write(json.dumps(events.json(), indent=4))
         outfile.close()
         git_commit([f"{year}_events.json"], commit_message)
     
     event_status = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/events/{year}/statuses?X-TBA-Auth-Key={api}')
-    # print(tba_api_key)
-    print(event_status.text)
+    # #print(tba_api_key)
+    #print(event_status.text)
     with open(f"{year}_event_statuses.json", "w") as outfile:
         outfile.write(json.dumps(event_status.json(), indent=4))
         outfile.close()
@@ -68,16 +68,16 @@ async def season(api):
         git_commit([f"{year}_event_statuses.json"], commit_message)
     
     awards = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/awards/{year}?X-TBA-Auth-Key={api}')
-    # print(tba_api_key)
-    print(awards.text)
+    # #print(tba_api_key)
+    #print(awards.text)
     with open(f"{year}_awards.json", "w") as outfile:
         outfile.write(json.dumps(awards.json(), indent=4))
         outfile.close()
         git_commit([f"{year}_awards.json"], commit_message)
     
     matches = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/matches/{year}?X-TBA-Auth-Key={api}')
-    # print(tba_api_key)
-    print(matches.text)
+    # #print(tba_api_key)
+    #print(matches.text)
     with open(f"{year}_matches.json", "w") as outfile:
         outfile.write(json.dumps(matches.json(), indent=4))
         outfile.close()
@@ -85,15 +85,15 @@ async def season(api):
     
 
     media = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc3461/media/{year}?X-TBA-Auth-Key={api}')
-    # print(tba_api_key)
-    print(media.text)
+    # #print(tba_api_key)
+    #print(media.text)
     with open(f"{year}_media.json", "w") as outfile:
         outfile.write(json.dumps(media.json(), indent=4))
         outfile.close()
         git_commit([f"{year}_media.json"], commit_message)
 
     districts = requests.get(f'https://www.thebluealliance.com/api/v3/district/{year}ne/rankings?X-TBA-Auth-Key={api}')
-    print(districts.text)
+    #print(districts.text)
     with open(f"{year}_district_rankings.json", "w") as outfile:
         outfile.write(json.dumps(districts.json(), indent=4))
         outfile.close()
@@ -127,9 +127,9 @@ while True:
 #     try:
 #         subprocess.run(["git", "add"] + files)
 #         subprocess.run(["git", "commit", "-m", message])
-#         print("Files committed successfully.")
+#         #print("Files committed successfully.")
 #     except Exception as e:
-#         print("Error committing files:", str(e))
+#         #print("Error committing files:", str(e))
 
 # List of files to commit
 # files_to_commit = ["file1.py", "file2.py"]
